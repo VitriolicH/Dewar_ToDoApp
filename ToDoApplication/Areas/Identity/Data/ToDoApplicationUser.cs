d9ae84cc4ff7eb5332f4ed92b845d27bf0998976
Mhamad <Mhamad01t@gmail.com>
2024-01-17 22:41:30 +0300

    Add project files.

---
+++ ToDoApplication/Areas/Identity/Data/ToDoApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApplication.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ToDoApplicationUser class
+public class ToDoApplicationUser : IdentityUser
+{
+    [Required(ErrorMessage = "Please Enter Your Name.")]
+    [DataType(DataType.Text)]
+    [Display(Name = "Name")]
+    public string UserName { get; set; } = string.Empty;
+}
+
---
+++ ToDoApplication/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApplication.Areas.Identity.Data;
+using ToDoApplication.Data;
+using Microsoft.AspNetCore.Identity;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ToDoContextConnection") ?? throw new InvalidOperationException("Connection string 'ToDoContextConnection' not found.");
+
+builder.Services.AddDbContext<ToDoContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ToDoApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ToDoContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
