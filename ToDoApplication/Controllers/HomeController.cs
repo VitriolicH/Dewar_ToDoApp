754cf60ee880f07b700cb03938e67e9164b622ff
Mhamad <Mhamad01t@gmail.com>
2024-01-19 17:00:14 +0300

    added create new task and showing each tast for each user

--- ToDoApplication/Controllers/HomeController.cs
+++ ToDoApplication/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using ToDoApplication.Areas.Identity.Data;
 using ToDoApplication.Data;
 using ToDoApplication.Models;
@@ -11,29 +10,66 @@ namespace ToDoApplication.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly UserManager<ToDoApplicationUser> _userManager;
+        private readonly ToDoContext cn;
+
         public IList<Task>? UserTasks { get; set; }
 
-        public HomeController(ILogger<HomeController> logger, UserManager<ToDoApplicationUser> userManager)
+        public HomeController(UserManager<ToDoApplicationUser> userManager, ToDoContext cn)
         {
-            _logger = logger;
             _userManager = userManager;
+            this.cn = cn;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int Id)
         {
             var userId = ViewData["UserId"] = _userManager.GetUserId(this.User);
-            
+            IQueryable<ToDoModel> Query = cn.Todos.Where(x => x.UserId == userId).OrderByDescending(t => t.Status);
 
-
-            return View();
+            return View(Query.ToList());
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpGet]
+        public IActionResult Add()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewData["UserId"] = _userManager.GetUserId(this.User);
+            var task = new ToDoModel();
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ToDoModel Task)
+        {
+            if (ModelState.IsValid)
+            {
+                cn.Todos.Add(Task);
+                cn.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult MarkAsComplete([FromRoute] int ID, ToDoModel CompletedTask)
+        {
+            CompletedTask = cn.Todos.SingleOrDefault(t => t.Id == CompletedTask.Id && t.UserId == _userManager.GetUserId(this.User))!;
+            if (CompletedTask != null)
+            {
+                CompletedTask.Status = "Done";
+                cn.SaveChanges();
+            }
+            return RedirectToAction("Index", new { Id = ID });
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCompletedTasks(int Id)
+        {
+            var ToDelete = cn.Todos.Where(t => t.Status == "Done" && t.UserId == _userManager.GetUserId(this.User));
+            foreach (var item in ToDelete)
+            {
+                cn.Remove(item);
+            }
+            cn.SaveChanges();
+            return RedirectToAction("Index", new { ID = Id });
         }
     }
 }
--- ToDoApplication/Models/ToDoModel.cs
+++ ToDoApplication/Models/ToDoModel.cs
@@ -17,11 +17,11 @@ public class ToDoModel
 
     [Required(ErrorMessage = "Please Select a Due Date ")]
     public DateTime DueDate { get; set; }
-    public string IsCompleted { get; set; } = "Pending";
+    public string Status { get; set; } = "Pending";
     public string UserId { get; set; } = string.Empty;
 
     [ValidateNever]
     public ToDoApplicationUser User { get; set; } = null!;
 
-    public bool TimeOver => IsCompleted == "Pending" && DueDate < DateTime.Today;
+    public bool TimeOver => Status == "Pending" && DueDate < DateTime.Today;
 }
